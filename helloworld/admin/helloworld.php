ede8488f46e4ca16b17fa8b00261fc38de68157c
Warwick Stone <uozu.aho@gmail.com>
2014-09-21 09:54:00 +1000

    Halfway through adding basic backend
    http://docs.joomla.org/Developing_a_Model-View-Controller_(MVC)_Component_for_Joomla!2.5_-_Part_07

--- helloworld/admin/helloworld.php
+++ helloworld/admin/helloworld.php
@@ -1,1 +1,19 @@
-hello world admin
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// import joomla controller library
+jimport('joomla.application.component.controller');
+
+// Get an instance of the controller prefixed by HelloWorld
+$controller = JController::getInstance('HelloWorld');
+
+// Get the task
+$jinput = JFactory::getApplication()->input;
+$task = $jinput->get('task', "", 'STR' );
+
+// Perform the Request task
+$controller->execute($task);
+
+// Redirect if set by the controller
+$controller->redirect();
--- todo.md
+++ todo.md
@@ -1,2 +1,6 @@
 todo
 ====
+
+Currently at
+
+http://docs.joomla.org/Developing_a_Model-View-Controller_(MVC)_Component_for_Joomla!2.5_-_Part_07
